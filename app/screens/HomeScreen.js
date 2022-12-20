1d0c28dfc7bde5648a504723cf6c43b8a72b3147
Danel Polyakov <danelpo@gmail.com>
2022-12-20 18:41:43 -0500

    added cat photos
    - Linked app to an api
    - Showing the cats in a grid
    	- 3 columns on web, 2 on mobile

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,20 +1,64 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Image,
+  FlatList,
+  Dimensions,
+  Platform,
+} from "react-native";
 
-function HomeScreen(props) {
-    return (
-        <View style={StyleSheet.homePageView}>
-            Home page
-        </View>
-    );
+function HomeScreen() {
+  const [cats, setCats] = useState("");
+
+  useEffect(() => {
+    const url =
+      "https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng";
+
+    const fetchCats = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setCats(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchCats();
+  }, []);
+
+  return (
+    <View style={styles.homePageView}>
+      <FlatList
+        data={cats}
+        style={styles.flatlistStyle}
+        numColumns={Platform.OS === "web" ? 3 : 2}
+        key={Platform.OS === "web" ? 3 : 2}
+        renderItem={({ item }) => (
+          <Image style={styles.catImage} source={{ uri: item.url }} />
+        )}
+      />
+    </View>
+  );
 }
 
+const listWidth = Dimensions.get("window").width * 0.9;
+const listMargin = Dimensions.get("window").width * 0.05;
+
 const styles = StyleSheet.create({
-    homePageView: {
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'light-gray'
-    }
-})
+  homePageView: {
+    flex: 1,
+    backgroundColor: "light-gray",
+  },
+  flatlistStyle: {
+    width: listWidth,
+    margin: listMargin,
+  },
+  catImage: {
+    width: listWidth / (Platform.OS === "web" ? 3 : 2),
+    aspectRatio: 1,
+  },
+});
 
 export default HomeScreen;
